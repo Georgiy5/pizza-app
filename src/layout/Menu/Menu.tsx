713ceb46585cb6f2151b1c5b33ca7f466b5a9f12
George <scherbinin.zhora2017@yandex.ru>
2025-06-26 15:29:31 +0300

    Outlet

---
+++ src/layout/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from 'react-router-dom';
+
+
+export function Layout () {
+	return <div>
+		<div>
+			<Link to='/'>Меню</Link>
+			<Link to='/cart'>Корзина</Link>
+		</div>
+		<div>
+			<Outlet/>
+		</div>
+	</div>;
+}
